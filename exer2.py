2f0a984c5d2715331f6297665acb78ed262b7243
Marcelo <marcelococcaro@email.com>
2024-04-12 18:02:38 -0300

    Programa que verifica se a pessoa gastou alem do orcamento

---
+++ exer2.py
@@ -0,0 +1,8 @@
+salrece=float(input("Digite o salário recebido:"))
+totalg= float(input("Digite o total gasto:"))
+
+if (totalg<=salrece):
+    print("Dentro do orçamento")
+
+else:
+    print("Fora do orçamento")
